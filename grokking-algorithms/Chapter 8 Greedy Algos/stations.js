3a9cfa2ed25bfdb0aa4bb6a4681075043661f4cd
Sophia Jung <sophiabraddock82@gmail.com>
2020-12-14 19:10:07 -0500

    chapter 8

---
+++ grokking-algorithms/Chapter 8 Greedy Algos/stations.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// You pass an array in, and it gets converted to a set.
+let statesNeeded = new Set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"]);
+
+const stations = {};
+stations["kone"] = new Set(["id", "nv", "ut"]);
+stations["ktwo"] = new Set(["wa", "id", "mt"]);
+stations["kthree"] = new Set(["or", "nv", "ca"]);
+stations["kfour"] = new Set(["nv", "ut"]);
+stations["kfive"] = new Set(["ca", "az"]);
+
+const finalStations = new Set();
+
+while (statesNeeded.size) {
+  let bestStation = null;
+  let statesCovered = new Set();
+  for (let station in stations) {
+    const states = stations[station];
+    const covered = new Set([...statesNeeded].filter(x => states.has(x)));
+    if (covered.size > statesCovered.size) {
+      bestStation = station;
+      statesCovered = covered;
+    }
+  }
+  statesNeeded = new Set([...statesNeeded].filter(x => !statesCovered.has(x)));
+  finalStations.add(bestStation);
+}
+
+console.log(finalStations); // $ node stations.js
+// Set(4) { 'kone', 'ktwo', 'kthree', 'kfive' }
